6fe812f14bec69619a37b6b81fb6969a09e86f91
timBrockman <tim@timbrockman.com>
2015-10-22 23:10:46 -0400

    feat: finished lesson 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,37 +1,91 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var name = 'Tim Brockman';
-var role = 'Developer';
 var bio = {
-	'name': name,
-	'role': role,
+	'name': 'Tim Brockman',
+	'role': 'Developer',
+	'welcomeMessage':'Hello',
+	'skills':['eat','drink','sleep'],
 	'contacts':{
 		'phone':'4076027721',
 		'email':'tim@timbrockman.com',
 		'github':'timBrockman',
-		'twitter':'timBrockman',
+		'linkedin':'timbrockman',
 		'location':'Celebration, FL'
 	},
 	'contact info':'tim@timbrockman.com',
 	'picture URL':'https://en.gravatar.com/userimage/13176978/d79b43787e69587972a0f83404db17fa.jpg?size=200',
-	'welcomeMessage':'lorem ipsum...',
-	'skills':['eat','drink','sleep']
 };
-var work = [
-	{
-		'employer':'',
-		'position':'',
-		'years worked':'',
-		'location':''
-	}];
-var education=[
-	{
-		'school':'',
-		'years':'',
-		'location':''
-	}];
+var projects = [{
+	'title':'',
+	'dates':'',
+	'description':'',
+	'url':'',
+	'images':[
+		{
+			'url':'',
+			'small':'',
+			'medium':'',
+			'alt':''
+		}
+	]
+}]
+var work = {
+	'jobs':[
+		{
+			'employer':'Republic Equity',
+			'title':'Startup Technologist',
+			'dates':'01/2013 - present',
+			'location':'Winter Park, FL',
+			'description':'work'
+		}]
+	};
+var education={
+	'schools':[
+		{
+			'school':'University of Central Florida',
+			'degree':'BFA Visual Arts',
+			'year':'2003',
+			'location':'Orlando, FL'
+		},
+		{
+			'school':'Udacity',
+			'degree':'ND Front End Developer',
+			'year':'Current',
+			'location':'Mountain View, CA'
+		}],
+	'onlineCourses':[
+		{
+			'title':'',
+			'school':'',
+			'dates':'',
+			'url':''
+		}]
+	};
+$('#header').append(HTMLheaderName)
+	.append(HTMLskillsStart);
+if(bio.skills.length > 0){
+	bio.skills.forEach(
+		function(d){
+			$('#skills').append(
+				 HTMLskills.replace('%data%',d));
 
+		}
+	);
+}
+//jobs in work
+for(k in work.jobs){
+	$('#workExperience').append(HTMLworkStart);
+	$('.work-entry:last').append(
+		HTMLworkEmployer.replace('%data%', work.jobs[k].employer) +
+		HTMLworkTitle.replace('%data%', work.jobs[k].title)+
+		HTMLworkDates.replace('%data%', work.jobs[k].dates)+
+		HTMLworkLocation.replace('%data%', work.jobs[k].location)+
+		HTMLworkDescription.replace('%data%', work.jobs[k].description));	
+}
+
+
+/* some test values
 $('#header').prepend(
 	HTMLheaderName.replace('%data%', bio['name']) +
 	'\n' +
@@ -40,4 +94,12 @@ $('#header').prepend(
 	HTMLcontactGeneric.replace('%contact%', 'contact info').replace('%data%', bio['contact info']) +
 	'\n' +
 	HTMLbioPic.replace('%data%', bio['picture URL']));
- 
+$('#workExperience').append(HTMLworkStart)
+	.append(
+		HTMLworkEmployer.replace('%data%', work['employer'])
+	);
+$('#education').append(HTMLschoolStart)
+	.append(
+		HTMLschoolName.replace('%data%', education[0].school)
+	);
+*/
